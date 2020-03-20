b172bb159175edbd22840be07b55ff209dd1ea71
Katerina Belousova <grantired.jpg@gmail.com>
2020-03-20 16:15:46 +0700

    Make some changes in main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include "split.h"
+#define N 256
+using namespace std;
+struct circle {
+    char figure[256];
+    int x, y;
+    double r;
+};
+int main()
+{
+    int num = 0;
+    cout << "Enter N = ";
+    cin >> num;
+    char tmp[N];
+    char tmp1[N];
+    for (int i = 0; i < num; i++) {
+        cin.clear();
+        while (cin.get() != '\n');
+        cout << "enter figure(x y, r)" << endl;
+        cin.getline(tmp, 100, '(');
+        cout << tmp << endl;
+        cin.getline(tmp1, 100,')');
+        cout << tmp1 << endl;
+        string str = string(tmp);
+        string coor = string(tmp1);
+        split(coor, ',');
+    }
+    
+    return 0;
+}
