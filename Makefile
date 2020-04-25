a60376520d26da861fd2dc9d1ddbd4ade0df1db7
Katerina Belousova <grantired.jpg@gmail.com>
2020-04-25 16:30:05 +0700

    Fix code after passed travis

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: circle
+all: 
 circle: 
 	g++ -g -Wall -Werror src/main.cpp -o build/main
 split:
--- src/split.h
+++ src/split.h
@@ -1,6 +1,6 @@
 #ifndef SPLIT_N
 #define SPLIT_H
-#include <vector>
 #include <string>
+#include <vector>
 std::vector<std::string> split(std::string str, char spl);
 #endif
